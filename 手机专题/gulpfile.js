4055c0d02475e3bce2bebab45fcb393b05b9b451
Hugh Zhao <lc.18@qq.com>
2017-03-19 01:39:14 +0800

    修改gulpfile,添加browers-sync支持

--- 手机专题/gulpfile.js
+++ 手机专题/gulpfile.js
@@ -1,27 +1,38 @@
-var gulp = require('gulp'),
-    less = require('gulp-less'),
-    px2rem = require('gulp-px2rem-plugin'),
-    mist = require('minimist');
+var gulp        = require('gulp');
+var mist        = require('minimist');
+var browserSync = require('browser-sync').create();
+var less        = require('gulp-less');
+var px2rem      = require('gulp-px2rem-plugin');
+var reload      = browserSync.reload;
 
+// 设置命令行变量参数 
 var folder = {
   string: 'dir',
   // default: { env: process.env.NODE_ENV || 'production' }
 };
-
 var folders = mist(process.argv.slice(2), folder);
 
-// gulp.task('default'[])
+// 静态服务器 + 监听 less/html 文件
+gulp.task('serve', ['less'], function() {
 
-gulp.task('less2css', function () {
-  var less_stream = gulp.src(folders.dir + '/css.less')
-    .pipe(px2rem())
-    .pipe(less())
-    .pipe(gulp.dest(folders.dir));
-  return less_stream;
+    browserSync.init({
+        // server: "./app"
+        server: folders.dir
+    });
+
+    // gulp.watch("app/less/*.less", ['less']);
+    // gulp.watch("app/*.html").on('change', reload);
+    gulp.watch(folders.dir + "/less/*.less", ['less']);
+    gulp.watch(folders.dir + "/*.html").on('change', reload);
 });
 
-gulp.task('px2rem',function(){
-  gulp.src(folders.dir + '/css.css')
-  .pipe(px2rem())
-
+// less编译后的css将注入到浏览器里实现更新
+gulp.task('less', function() {
+    return gulp.src(folders.dir + "/less/*.less")
+        .pipe(px2rem())
+        .pipe(less())
+        .pipe(gulp.dest(folders.dir + "/css"))
+        .pipe(reload({stream: true}));
 });
+
+gulp.task('default', ['serve']);
